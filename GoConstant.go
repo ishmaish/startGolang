6279768ec30943f4b7c1c89cdbd48afce3518ad8
ishmaish <ishma.ish19@gmail.com>
2024-03-19 13:06:41 +0530

    constants in GOLANG

---
+++ GoConstant.go
@@ -0,0 +1,17 @@
+package main
+
+// declaring a group of constants
+const (
+	PRODUCT  = "Mobile"
+	QUANTITY = 50
+	PRICE    = 50.50
+	STOCK    = true
+)
+
+func main() {
+	// A constant is a name or an identifier for a fixed value
+	// you must assign the value at the time of declaration
+
+	const NAME = "ISHMA"
+
+}
